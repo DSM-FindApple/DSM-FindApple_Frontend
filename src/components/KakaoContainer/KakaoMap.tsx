2f193a05228315da7baca22b929cab8a1d1cc592
oajh04 <cloudtubekr@naver.com>
2021-09-29 23:34:49 +0900

    ⚡️ :: kakao map 띄우기

--- src/components/KakaoContainer/KakaoMap.tsx
+++ src/components/KakaoContainer/KakaoMap.tsx
@@ -1,14 +1,83 @@
 import React from 'react';
+import { useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+import KakaoMarker from './KakaoMarker';
+
+// const positions = [
+//     {
+//       title: "카카오",
+//       latlng: { lat: 33.450705, lng: 126.570677 },
+//     },
+//     {
+//       title: "생태연못",
+//       latlng: { lat: 33.450936, lng: 126.569477 },
+//     },
+//     {
+//       title: "텃밭",
+//       latlng: { lat: 33.450879, lng: 126.56994 },
+//     },
+//     {
+//       title: "근린공원",
+//       latlng: { lat: 33.451393, lng: 126.570738 },
+//     },
+//   ]
+
+const markerdata = [
+    {
+      title: "대전역",
+      lat: 36.33248155546412,
+      lng: 127.4342002693787,
+    },
+    {
+      title: "천리집",
+      lat: 36.39496683963465,
+      lng: 127.35156630094322,
+    },
+    {
+      title: "감동헤어",
+      lat: 36.38941522030629,
+      lng: 127.34884929417515,
+    },
+  ];
 
 const KakaoMap = () => {
-    const { kakao } = window;
-    const mapDiv = document.querySelector('#map')
-    const options = {
-        center: new kakao.maps.LatLng(37.62197524055062, 127.16017523675508),
-        level: 2,
-    };
-    const map = new kakao.maps.Map(mapDiv, options);
-    return (map);
+    const [ seleteMarker ,setSeleteMarker ] = useState<number>()
+
+    return (
+        <Map // 지도를 표시할 Container
+        center={{
+          // 지도의 중심좌표
+          lat: 36.39155703543644, 
+          lng: 127.3633693928144,
+        }}
+        style={{
+          // 지도의 크기
+          width: "100vw",
+          height: "100vh",
+        }}
+        level={3} // 지도의 확대 레벨
+        >
+            {
+                markerdata.map((data, index) => (
+                    <>
+                        <KakaoMarker 
+                            lat={data.lat} 
+                            lng={data.lng} 
+                            index={index}
+                            title={data.title}
+                            onClick={() => setSeleteMarker(index)}
+                            isClick={seleteMarker === index}
+                            key={`EventMarkerContainer-${data.lat}-${data.lng}`}
+                        ></KakaoMarker>
+                        {
+                            console.log(data)
+                        }
+                    </>
+                ))
+            }
+        </Map>
+        
+    )
 }
 
 export default KakaoMap;
