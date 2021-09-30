d990c26e609143ef19c6e62f3041a66779c8890d
oajh04 <cloudtubekr@naver.com>
2021-09-30 22:25:00 +0900

    ⚡️ :: globalstyle 추가

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+
+export const GlobalStyle = css`
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    a{
+        text-decoration: none;
+    }
+`
