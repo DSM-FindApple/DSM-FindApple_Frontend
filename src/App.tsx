68e8c7e1b3afdde073c97f16bfd68366bf0fe070
oajh04 <cloudtubekr@naver.com>
2021-09-30 22:25:46 +0900

    ⚡️ :: App 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Global } from '@emotion/react';
 import React from 'react';
 import './App.css';
 import { Location } from './components';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <Global styles={GlobalStyle} />
       <Location />
     </>
   );
