756c8e6b702e28d41b66f4455cff16b41879029e
oajh04 <cloudtubekr@naver.com>
2021-10-01 23:11:37 +0900

    ⚡️ :: marker -> Marker naming 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,3 @@
 export { default as SpeechBubble } from './SpeechBubble.svg'
 export { default as logo } from './logo.svg'
-export { default as marker } from './marker.svg'
+export { default as marker } from './Marker.svg'
