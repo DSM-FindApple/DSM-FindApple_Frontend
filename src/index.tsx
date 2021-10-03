b91a85a80c3d4debccc5cea9145ce886e818a70c
oajh04 <cloudtubekr@naver.com>
2021-10-03 23:10:12 +0900

    :coffin: :: index 에서 redux 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { rootReducer } from './Redux/Reducers/rootReducer';
 
 // const devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
 
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
