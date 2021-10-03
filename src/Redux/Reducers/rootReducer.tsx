b197820b343ab4b1e13cd8b01dba48beb773d65a
oajh04 <cloudtubekr@naver.com>
2021-10-03 23:09:55 +0900

    :coffin: :: RootReducer 삭제

--- src/Redux/Reducers/rootReducer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { combineReducers } from 'redux';
-import modalReducer from './Modal'
-
-export const rootReducer = combineReducers({
-    modalReducer
-})
-
-export type RootState = ReturnType<typeof rootReducer>
