a3f446699b6709c17cde80369ca3bb1c3c1ddece
oajh04 <cloudtubekr@naver.com>
2021-10-03 23:10:42 +0900

    ⚡️ :: App.css, router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Global } from '@emotion/react';
-import { Location } from './components';
 import { GlobalStyle } from './styles/GlobalStyles';
+import './App.css';
+import RootRouter from './Router/RootRouter';
 
 function App() {
   return (
     <>
       <Global styles={GlobalStyle} />
-      <Location />
+      <RootRouter />
     </>
   );
 }
