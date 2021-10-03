8d998345ebf8f5adc7212597f034cf688178c099
oajh04 <cloudtubekr@naver.com>
2021-10-03 23:08:19 +0900

    ⚡️ :: Global style

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,9 +2,13 @@ import { css } from "@emotion/react";
 
 
 export const GlobalStyle = css`
+
     body {
         margin: 0;
         padding: 0;
+        box-sizing: border-box;
+        background-color: #FAFAFA;
+        outline: none;
     }
     a{
         text-decoration: none;
