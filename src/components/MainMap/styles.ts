999bc0dc70c6fafae6087baea8d9c5dccf36466a
oajh04 <cloudtubekr@naver.com>
2021-10-03 23:12:47 +0900

    ⚡️ :: MainMap style

---
+++ src/components/MainMap/styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const Warpper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 500px;
+  margin: 0 auto;
+`
