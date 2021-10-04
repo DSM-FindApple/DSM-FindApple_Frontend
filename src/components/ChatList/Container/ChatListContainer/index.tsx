c0aca5fc85befc1ec9a04299412ccb0d67a099e1
oajh04 <cloudtubekr@naver.com>
2021-10-04 22:33:34 +0900

    ⚡️ :: ChatList Container

---
+++ src/components/ChatList/Container/ChatListContainer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from './styles'
+
+const ChatListContainer = () => {
+  return (
+    <>
+        <S.ContainerWrapper>
+            {/* <img src={} alt="asd"> */}
+            <div style={{width: '40px', height: "40px", borderRadius: '50%', backgroundColor: "blue"}}></div>
+        </S.ContainerWrapper>
+    </>
+  );
+}
+
+export default ChatListContainer;
