b6bd7df1c6f09d0e40ddb0e3d1289e069c05232d
oajh04 <cloudtubekr@naver.com>
2021-10-04 22:33:11 +0900

    ⚡️ :: ChatList style

---
+++ src/components/ChatList/styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+    max-width: 500px;
+    width: 100%;
+    height: 100vh;
+    margin: 0 auto;
+    background-color: white;
+`
