05cac10ba8b018c28c1348523fab0650a943a4c5
oajh04 <cloudtubekr@naver.com>
2021-10-13 22:26:06 +0900

    ⚡️ :: Router에 chat 넣음

--- src/Router/MainRouter.tsx
+++ src/Router/MainRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import { 
+  Chat,
   ChatList,
   MainMap,
  } from '../components';
@@ -13,6 +14,7 @@ const MainRouter = () => {
         <Switch>
           <Route path='/' component={MainMap} exact/>
           <Route path='/chatlist' component={ChatList} exact/>
+          <Route path='/chat/:chatId' component={Chat} exact />
         </Switch>
     </>
   );
